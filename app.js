e0ab9f655e8dc317841044808bc2dfc1714cab4e
mohammadarshid <mohammadarshid3@gmail.com>
2026-06-02 23:35:03 +0530

    fix: remove duplicate code

--- app.js
+++ app.js
@@ -4,34 +4,27 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectdb = require("./config/db");
-// rest object
 
-// confg
 dotenv.config();
 
-//db connection
 connectdb();
+
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// route
 app.use("/api/v1/test", require("./routes/testRoutes"));
 app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/resturant", require("./routes/resturantroute"));
 app.use("/api/v1/category", require("./routes/categeoryroute"));
 app.use("/api/v1/foodcategory", require("./routes/foodroutes"));
-app.get("/", (req, res) => {
-  return res
-    .status(200)
-    .json("<h1>welcome to food Server  App api server </h1>");
-});
 
-// port
+app.get("/", (req, res) => {
+  return res.status(200).json("<h1>welcome to food Server App api server</h1>");
+});
 
 const PORT = process.env.PORT || 8080;
 
